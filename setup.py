b156c68ff62661f1c2110a0ed72da9375cb402ea
tv <tv@localhost.localdomain>
2014-11-25 11:59:25 +0000

    Packaged simple_units.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='simple_units',
+      version='0.1',
+      description='Add units to Python',
+      packages=find_packages(exclude=['test*']),
+      install_requires=[]
+     )
